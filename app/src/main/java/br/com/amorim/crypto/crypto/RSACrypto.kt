17571c72523e1c6dae43014f95e84ddc7ed51405
Gabriel Amorim <gsilveiraamorim@gmail.com>
2020-12-31 15:42:05 -0300

    add hability to work with large texts with encrypt and decrypt
    Signed-off-by: Gabriel Amorim <gsilveiraamorim@gmail.com>

--- app/src/main/java/br/com/amorim/crypto/crypto/RSACrypto.kt
+++ app/src/main/java/br/com/amorim/crypto/crypto/RSACrypto.kt
@@ -3,6 +3,7 @@ package br.com.amorim.crypto.crypto
 import android.content.Context
 import android.util.Base64
 import br.com.amorim.crypto.keys.RSAKey
+import java.io.ByteArrayOutputStream
 import javax.crypto.Cipher
 
 class RSACrypto(
@@ -12,30 +13,71 @@ class RSACrypto(
         initAndGenerateKeyPair()
     }
 
+    private val encryptCipher: Cipher by lazy {
+        Cipher.getInstance(CIPHER_RSA_ENCRYPT_MODE).apply {
+            init(Cipher.ENCRYPT_MODE, rsaKey.getPrivateKey())
+        }
+    }
+
+    private val decryptCipher: Cipher by lazy {
+        Cipher.getInstance(CIPHER_RSA_ENCRYPT_MODE).apply {
+            init(Cipher.DECRYPT_MODE, rsaKey.getPublicKey())
+        }
+    }
+
     fun encrypt(value: String): String {
-        val cipher: Cipher = Cipher.getInstance(CIPHER_RSA_ENCRYPT_MODE)
-        cipher.init(Cipher.ENCRYPT_MODE, rsaKey.getPrivateKey())
-
-        val originalMessage = value.toByteArray() // Converte a mensagem original para byteArray
-        val encryptedMessage = cipher.doFinal(originalMessage) //Mensagem em byteArray criptografada
-        val encryptMessageString = Base64.encodeToString(encryptedMessage, Base64.DEFAULT)
-
-        return encryptMessageString
+        val originalMessage = value.toByteArray()
+        return if (originalMessage.size > CIPHER_BLOCK_SIZE) {
+            encryptLargeText(encryptCipher, originalMessage)
+        } else {
+            Base64.encodeToString(encryptCipher.doFinal(value.toByteArray()), Base64.DEFAULT)
+        }
     }
 
     fun decrypt(value: String): String {
-        val cipher: Cipher = Cipher.getInstance(CIPHER_RSA_ENCRYPT_MODE)
-        cipher.init(Cipher.DECRYPT_MODE, rsaKey.getPublicKey())
-
-        //Encoda em base64 a mensagem criptograda
         val encrypted = Base64.decode(value, Base64.DEFAULT)
-        //Remove a criptografia
-        val decrypted = cipher.doFinal(encrypted)
-        //Converte para string a mensagem em byteArray
-        return String(decrypted)
+        return if (encrypted.size > CIPHER_BLOCK_SIZE)
+            decryptLargeText(decryptCipher, encrypted)
+        else {
+            String(decryptCipher.doFinal(Base64.decode(value, Base64.DEFAULT)))
+        }
+    }
+
+    private fun encryptLargeText(cipher: Cipher, message: ByteArray): String {
+        // k - 11 octets (k is the octet length of the RSA modulus) k -> KeySize/8
+        var limit: Int = (RSAKey.RSA_KEY_SIZE / 8) - 11
+        var position = 0
+
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        while (position < message.size) {
+            if (message.size - position < limit) limit = message.size - position
+            val res = cipher.doFinal(message, position, limit)
+            byteArrayOutputStream.write(res)
+            position += limit
+        }
+
+        return Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT)
+    }
+
+    private fun decryptLargeText(cipher: Cipher, encryptedMessage: ByteArray): String {
+        var limit = RSAKey.RSA_KEY_SIZE / 8
+        var position = 0
+
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        while (position < encryptedMessage.size) {
+            if (encryptedMessage.size - position < limit) {
+                limit = encryptedMessage.size - position
+            }
+            val result = cipher.doFinal(encryptedMessage, position, limit)
+            byteArrayOutputStream.write(result)
+            position += limit
+        }
+
+        return String(byteArrayOutputStream.toByteArray())
     }
 
     companion object {
         private const val CIPHER_RSA_ENCRYPT_MODE = "RSA/ECB/PKCS1Padding"
+        private const val CIPHER_BLOCK_SIZE = 256
     }
 }
--- app/src/main/java/br/com/amorim/crypto/keys/RSAKey.kt
+++ app/src/main/java/br/com/amorim/crypto/keys/RSAKey.kt
@@ -3,10 +3,12 @@
 package br.com.amorim.crypto.keys
 
 import android.content.Context
+import android.os.Build
 import android.security.KeyPairGeneratorSpec
 import android.security.KeyPairGeneratorSpec.Builder
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
+import androidx.annotation.RequiresApi
 import java.math.BigInteger
 import java.security.KeyPair
 import java.security.KeyPairGenerator
@@ -27,28 +29,24 @@ class RSAKey(
     }
 
     fun initAndGenerateKeyPair() {
-        val spec =
-            Builder(context).run {
-                setKeySize(RSA_KEY_SIZE)
-                setSerialNumber(BigInteger.TEN)
-                setSubject(X500Principal("CN=$ALIAS"))
-                setAlias(ALIAS)
-                setStartDate(Calendar.getInstance().time)
-                setEndDate(Calendar.getInstance().apply { add(Calendar.YEAR, 2) }.time)
-                build()
-            }
+        if (keyStore?.containsAlias(ALIAS) == true) {
+            // We will not ask to generate every single time
+            return
+        }
 
-        KeyPairGenerator.getInstance(
-            RSA_ENCRYPT_MODE,
-            ANDROID_KEY_STORE
-        ).apply {
-            initialize(spec)
-            generateKeyPair()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            generatePairKeyAfterAndroidM()
+        } else {
+            generatePairKeyBeforeAndroidM()
         }
     }
 
     fun getPrivateKey(): PrivateKey? {
-        return (keyStore?.getEntry(ALIAS, null) as? PrivateKeyEntry)?.privateKey
+        return if (Build.VERSION.SDK_INT == Build.VERSION_CODES.P) {
+            keyStore?.getKey(ALIAS, null) as PrivateKey
+        } else {
+            (keyStore?.getEntry(ALIAS, null) as? PrivateKeyEntry)?.privateKey
+        }
     }
 
     fun getPublicKey(): PublicKey? {
@@ -56,10 +54,62 @@ class RSAKey(
         return privateKeyEntry?.certificate?.publicKey
     }
 
+    @Suppress("DEPRECATION")
+    private fun generatePairKeyBeforeAndroidM() {
+        val specBuilder = KeyPairGeneratorSpec.Builder(context).run {
+            setKeySize(RSA_KEY_SIZE)
+            setSerialNumber(BigInteger.TEN)
+            setSubject(X500Principal("CN=$ALIAS"))
+            setAlias(ALIAS)
+            setStartDate(Calendar.getInstance().apply { add(Calendar.DAY_OF_YEAR, -1) }.time)
+            setEndDate(Calendar.getInstance().apply { add(Calendar.YEAR, 2) }.time)
+            build()
+        }
+
+        KeyPairGenerator.getInstance(
+            RSA_ENCRYPT_MODE,
+            ANDROID_KEY_STORE
+        ).apply {
+            initialize(specBuilder)
+            generateKeyPair()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun generatePairKeyAfterAndroidM() {
+        val kpg: KeyPairGenerator = KeyPairGenerator.getInstance(
+            KeyProperties.KEY_ALGORITHM_RSA,
+            ANDROID_KEY_STORE
+        )
+        val parameterSpec: KeyGenParameterSpec = KeyGenParameterSpec.Builder(
+            ALIAS,
+            KeyProperties.PURPOSE_ENCRYPT or
+                KeyProperties.PURPOSE_DECRYPT or
+                KeyProperties.PURPOSE_SIGN or
+                KeyProperties.PURPOSE_VERIFY
+        ).run {
+            setDigests(KeyProperties.DIGEST_MD5, KeyProperties.DIGEST_NONE, KeyProperties.DIGEST_SHA1, KeyProperties.DIGEST_SHA224, KeyProperties.DIGEST_SHA256, KeyProperties.DIGEST_SHA384, KeyProperties.DIGEST_SHA512)
+            setKeySize(RSA_KEY_SIZE)
+            setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_PKCS1)
+            setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PKCS1)
+            setCertificateSerialNumber(BigInteger.TEN)
+            setCertificateSubject(X500Principal("CN=$ALIAS"))
+            setKeyValidityStart(Calendar.getInstance().apply { add(Calendar.DAY_OF_YEAR, -1) }.time)
+            setKeyValidityEnd(Calendar.getInstance().apply { add(Calendar.YEAR, 2) }.time)
+            build()
+        }
+
+        kpg.apply {
+            initialize(parameterSpec)
+            generateKeyPair()
+        }
+    }
+
     companion object {
         private const val RSA_ENCRYPT_MODE = "RSA"
-        private const val RSA_KEY_SIZE = 2048
         private const val ANDROID_KEY_STORE = "AndroidKeyStore"
         private const val ALIAS = "alias"
+
+        const val RSA_KEY_SIZE = 2048
     }
 }
